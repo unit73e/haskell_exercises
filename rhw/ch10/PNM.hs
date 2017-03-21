f42d41b14659ef2528bbd2cfaa9434db79f70a43
Emanuel Amaral Couto <unit73e@gmail.com>
2017-03-21 20:18:20 +0000

    Code refactor

--- rhw/ch10/PNM.hs
+++ rhw/ch10/PNM.hs
@@ -5,6 +5,7 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Char (isSpace)
 import System.Environment (getArgs)
 
+-- | A portable grey map (PGM)
 data Greymap = Greymap
   { greyWidth :: Int
   , greyHeight :: Int
@@ -16,49 +17,79 @@ instance Show Greymap where
   show (Greymap w h m _) =
     "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m
 
-matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
-matchHeader prefix str
+-- | Return the suffix of the second string if its prefix matches
+-- the entire first string.
+--
+-- Examples:
+--
+-- > stripPrefix "foo" "foobar" == Just "bar"
+-- > stripPrefix ""    "baz"    == Just "baz"
+-- > stripPrefix "foo" "quux"   == Nothing
+stripPrefix :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+stripPrefix prefix str
   | prefix `L8.isPrefixOf` str =
-    Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
+    let afterMagicNumber = L.drop (L.length prefix) str
+    in Just $ L8.dropWhile isSpace afterMagicNumber
   | otherwise = Nothing
 
--- "nat" here is short for "natural number"
-getNat :: L.ByteString -> Maybe (Int, L.ByteString)
-getNat s =
+-- | Reads a natural number from the beginning of the string.
+-- A natural number is a positive integer (does not include zero).
+-- If there is no natural number at the beginning of the string,
+-- returns Nothing, otherwise returns the natural number and the
+-- rest of the string.
+--
+-- > readNat "5cards" == Just (5,"cards")
+-- > readNat "cards"  == Nothing
+-- > readNat "0cards" == Nothing
+readNat :: L.ByteString -> Maybe (Int, L.ByteString)
+readNat s =
   case L8.readInt s of
     Nothing -> Nothing
-    Just (num, rest)
-      | num <= 0 -> Nothing
-      | otherwise -> Just (fromIntegral num, rest)
+    j@(Just (n, _))
+      | n <= 0 -> Nothing
+      | otherwise -> j
 
-getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
-getBytes n str =
+
+-- | Returns a tuple where the first element is a prefix of length @n@
+-- and the second element is the remainder of the string. If @n@ exceedes
+-- the size of the string, return @Nothing@.
+--
+-- Examples:
+-- > readBytes 3 "abcdef"    == Just ("abc","def")
+-- > readBytes 6 "abcdef"    == Just ("abcdef","")
+-- > readBytes 0 "abcdef"    == Just ("","abcdef")
+-- > readBytes (-2) "abcdef" == Just ("","abcdef")
+-- > readBytes 8 "abcdef"    == Nothing
+readBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+readBytes n str =
   let count = fromIntegral n
       both@(prefix, _) = L.splitAt count str
   in if L.length prefix < count
        then Nothing
        else Just both
 
+-- | Translates a PGM string into a @Greymap@ or returns @Nothing@
+-- if the string is not a PGM
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
 parseP5 s =
-  case matchHeader "P5" s of
+  case stripPrefix "P5" s of
     Nothing -> Nothing
     Just s1 ->
-      case getNat s1 of
+      case readNat s1 of
         Nothing -> Nothing
         Just (width, s2) ->
-          case getNat (L8.dropWhile isSpace s2) of
+          case readNat (L8.dropWhile isSpace s2) of
             Nothing -> Nothing
             Just (height, s3) ->
-              case getNat (L8.dropWhile isSpace s3) of
+              case readNat (L8.dropWhile isSpace s3) of
                 Nothing -> Nothing
                 Just (maxGrey, s4)
                   | maxGrey > 255 -> Nothing
                   | otherwise ->
-                    case getBytes 1 s4 of
+                    case readBytes 1 s4 of
                       Nothing -> Nothing
                       Just (_, s5) ->
-                        case getBytes (width * height) s5 of
+                        case readBytes (width * height) s5 of
                           Nothing -> Nothing
                           Just (bitmap, s6) ->
                             Just (Greymap width height maxGrey bitmap, s6)
--- rhw/ch10/PNM2.hs
+++ rhw/ch10/PNM2.hs
@@ -5,6 +5,7 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Char (isSpace)
 import System.Environment (getArgs)
 
+-- | A portable grey map (PGM)
 data Greymap = Greymap
   { greyWidth :: Int
   , greyHeight :: Int
@@ -16,48 +17,86 @@ instance Show Greymap where
   show (Greymap w h m _) =
     "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m
 
-matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
-matchHeader prefix str
+-- | Return the suffix of the second string if its prefix matches
+-- the entire first string.
+--
+-- Examples:
+--
+-- > stripPrefix "foo" "foobar" == Just "bar"
+-- > stripPrefix ""    "baz"    == Just "baz"
+-- > stripPrefix "foo" "quux"   == Nothing
+stripPrefix :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+stripPrefix prefix str
   | prefix `L8.isPrefixOf` str =
-    Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
+    let afterMagicNumber = L.drop (L.length prefix) str
+    in Just $ L8.dropWhile isSpace afterMagicNumber
   | otherwise = Nothing
 
--- "nat" here is short for "natural number"
-getNat :: L.ByteString -> Maybe (Int, L.ByteString)
-getNat s =
+-- | Reads a natural number from the beginning of the string.
+-- A natural number is a positive integer (does not include zero).
+-- If there is no natural number at the beginning of the string,
+-- returns Nothing, otherwise returns the natural number and the
+-- rest of the string.
+--
+-- > readNat "5cards" == Just (5,"cards")
+-- > readNat "cards"  == Nothing
+-- > readNat "0cards" == Nothing
+readNat :: L.ByteString -> Maybe (Int, L.ByteString)
+readNat s =
   case L8.readInt s of
     Nothing -> Nothing
-    Just (num, rest)
-      | num <= 0 -> Nothing
-      | otherwise -> Just (fromIntegral num, rest)
+    j@(Just (n, _))
+      | n <= 0 -> Nothing
+      | otherwise -> j
 
-getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
-getBytes n str =
+
+-- | Returns a tuple where the first element is a prefix of length @n@
+-- and the second element is the remainder of the string. If @n@ exceedes
+-- the size of the string, return @Nothing@.
+--
+-- Examples:
+-- > readBytes 3 "abcdef"    == Just ("abc","def")
+-- > readBytes 6 "abcdef"    == Just ("abcdef","")
+-- > readBytes 0 "abcdef"    == Just ("","abcdef")
+-- > readBytes (-2) "abcdef" == Just ("","abcdef")
+-- > readBytes 8 "abcdef"    == Nothing
+readBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+readBytes n str =
   let count = fromIntegral n
       both@(prefix, _) = L.splitAt count str
   in if L.length prefix < count
        then Nothing
        else Just both
 
+-- | Removes the prefix spaces from the right element of the tuple.
+--
+-- Examples:
+-- > skipSpaces ('a',"   a")     == Just ('a',"a")
+-- > skipSpaces ('a',"   a    ") == Just ('a',"a    ")
+-- > skipSpaces ('a',"        ") == Just ('a',"")
+skipSpaces :: (a, L.ByteString) -> Maybe (a, L.ByteString)
+skipSpaces (a, s) = Just (a, L8.dropWhile isSpace s)
+
+-- | Compose two actions, passing the value of produced by the first
+-- as an argument of the second.
 (>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
 Nothing >>? _ = Nothing
 Just v >>? f = f v
 
+-- | Translates a PGM string into a @Greymap@ or returns @Nothing@
+-- if the string is not a PGM
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
 parseP5 s =
-    matchHeader "P5" s                >>?
-    \s -> skipSpace ((), s)           >>?
-    (getNat . snd)                    >>?
-    skipSpace                         >>?
-    \(width, s) ->   getNat s         >>?
-    skipSpace                         >>?
-    \(height, s) ->  getNat s         >>?
-    \(maxGrey, s) -> getBytes 1 s     >>?
-    (getBytes (width * height) . snd) >>?
+    stripPrefix "P5" s                >>?
+    \s -> skipSpaces ((), s)           >>?
+    (readNat . snd)                    >>?
+    skipSpaces                         >>?
+    \(width, s) ->   readNat s         >>?
+    skipSpaces                         >>?
+    \(height, s) ->  readNat s         >>?
+    \(maxGrey, s) -> readBytes 1 s     >>?
+    (readBytes (width * height) . snd) >>?
     \(bitmap, s) -> Just (Greymap width height maxGrey bitmap, s)
-
-skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
-skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
 
 main :: IO ()
 main = do
