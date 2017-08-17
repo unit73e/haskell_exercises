1b01eb7972329016224c9b564fb02ec1a9f79b7a
Emanuel Amaral Couto <unit73e@gmail.com>
2017-08-17 20:55:00 +0100

    Update chapter 10

--- rhw/ch10/PGM3.hs
+++ rhw/ch10/PGM3.hs
@@ -3,10 +3,11 @@
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Word (Word8)
-import Data.Char (isSpace)
+import Data.Char (isSpace, isDigit, chr)
 import Data.Int (Int64)
 import System.Environment (getArgs)
 import PGM(Graymap(..))
+import Control.Applicative ((<$>))
 
 -- | A parse state
 data ParseState = ParseState
@@ -18,9 +19,43 @@ newtype Parse a = Parse
   { runParse :: ParseState -> Either String (a, ParseState)
   }
 
--- | Set a new offset
-modifyOffset :: ParseState -> Int64 -> ParseState
-modifyOffset initState newOffset = initState {offset = newOffset}
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+notWhite :: Char -> Bool
+notWhite = (`L8.notElem` " \r\n\t")
+
+-- | Returns the current parsing state
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+-- | Replaces the current parsing state
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+-- | Terminates parsing and returns an error
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
+
+assert :: Bool -> String -> Parse ()
+assert True _ = identity ()
+assert False err = bail err
+
+-- | Chains parsers together
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+  where
+    chainedParser initState =
+      case runParse firstParser initState of
+        Left errMessage -> Left errMessage
+        Right (firstResult, newState) ->
+          runParse (secondParser firstResult) newState
+
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> const f
+
+instance Functor Parse where
+  fmap f parser = parser ==> \result -> identity (f result)
 
 -- | Injects a value into a parser
 identity :: a -> Parse a
@@ -35,107 +70,51 @@ parseByte =
         where newState = initState {string = remainder, offset = newOffset}
               newOffset = offset initState + 1
 
--- | Returns the current parsing state
-getState :: Parse ParseState
-getState = Parse (\s -> Right (s, s))
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
 
--- | Replaces the current parsing state
-putState :: ParseState -> Parse ()
-putState s = Parse (\_ -> Right ((), s))
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p =
+  (fmap p <$> peekByte) ==> \mp ->
+    if mp == Just True
+      then parseByte ==> \b -> (b :) <$> parseWhile p
+      else identity []
 
--- | Terminates parsing and returns an error
-bail :: String -> Parse a
-bail err = Parse $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
 
--- | Chains parsers together
-(==>) :: Parse a -> (a -> Parse b) -> Parse b
-firstParser ==> secondParser = Parse chainedParser
-  where
-    chainedParser initState =
-      case runParse firstParser initState of
-        Left errMessage -> Left errMessage
-        Right (firstResult, newState) ->
-          runParse (secondParser firstResult) newState
+parseNat :: Parse Int
+parseNat =
+  parseWhileWith w2c isDigit ==> \digits ->
+    if null digits
+      then bail "no more input"
+      else let n = read digits
+           in if n < 0
+                then bail "integer overflow"
+                else identity n
 
--- | Parses a PGM string
-parse :: Parse a -> L.ByteString -> Either String a
-parse parser initState =
-  case runParse parser (ParseState initState 0) of
-    Left err -> Left err
-    Right (result, _) -> Right result
+parseBytes :: Int -> Parse L.ByteString
+parseBytes n =
+  getState ==> \st ->
+    let n' = fromIntegral n
+        (h, t) = L.splitAt n' (string st)
+        st' = st {offset = offset st + L.length h, string = t}
+    in putState st' ==>& assert (L.length h == n') "end of input" ==>&
+       identity h
 
--- | Return the suffix of the second string if its prefix matches
--- the entire first string.
---
--- Examples:
---
--- > stripPrefix "foo" "foobar" == Just "bar"
--- > stripPrefix ""    "baz"    == Just "baz"
--- > stripPrefix "foo" "quux"   == Nothing
-stripPrefix :: L.ByteString -> L.ByteString -> Maybe L.ByteString
-stripPrefix prefix str
-  | prefix `L8.isPrefixOf` str = Just suffix
-  | otherwise = Nothing
-  where
-    suffix = L.drop (L.length prefix) str
+parseP5 :: Parse Graymap
+parseP5 =
+  parseWhileWith w2c notWhite ==> \header -> skipSpaces ==>&
+  assert (header == "P5") "invalid raw header" ==>&
+  parseNat ==> \width -> skipSpaces ==>&
+  parseNat ==> \height -> skipSpaces ==>&
+  parseNat ==> \maxGrey ->
+  parseByte ==>&
+  parseBytes (width * height) ==> \bitmap ->
+  identity (Graymap width height maxGrey bitmap)
 
--- | Reads a natural number from the beginning of the string.
--- A natural number is a positive integer (does not include zero).
--- If there is no natural number at the beginning of the string,
--- returns Nothing, otherwise returns the natural number and the
--- rest of the string.
---
--- > readNat "5cards" == Just (5,"cards")
--- > readNat "cards"  == Nothing
--- > readNat "0cards" == Nothing
-readNat :: L.ByteString -> Maybe (Int, L.ByteString)
-readNat s =
-  case L8.readInt s of
-    Nothing -> Nothing
-    j@(Just (n, _))
-      | n <= 0 -> Nothing
-      | otherwise -> j
-
-
--- | Returns a tuple where the first element is a prefix of length @n@
--- and the second element is the remainder of the string. If @n@ exceedes
--- the size of the string, return @Nothing@.
---
--- Examples:
--- > readBytes 3 "abcdef"    == Just ("abc","def")
--- > readBytes 6 "abcdef"    == Just ("abcdef","")
--- > readBytes 0 "abcdef"    == Just ("","abcdef")
--- > readBytes (-2) "abcdef" == Just ("","abcdef")
--- > readBytes 8 "abcdef"    == Nothing
-readBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
-readBytes n str =
-  let count = fromIntegral n
-      both@(prefix, _) = L.splitAt count str
-  in if L.length prefix < count
-       then Nothing
-       else Just both
-
--- | Returns the given string without prefixed spaces.
---
--- Examples:
--- > skipSpaces "          foo" == "foo"
--- > skipSpaces "\t\n\r\f\vfoo" == "foo"
--- > skipSpaces "bar          " == "bar          "
--- > skipSpaces "             " == ""
-skipSpaces :: L.ByteString -> L.ByteString
-skipSpaces = L8.dropWhile isSpace
-
--- | Translates a PGM string into a @Graymap@ or returns @Nothing@
--- if the string is not a PGM
-parseP5 :: L.ByteString -> Maybe (Graymap, L.ByteString)
-parseP5 s = do
-    s1            <- stripPrefix "P5" s
-    (width, s2)   <- readNat (skipSpaces s1)
-    (height, s3)  <- readNat (skipSpaces s2)
-    (maxGray, s4) <- readNat (skipSpaces s3)
-    (_, s5)       <- readBytes 1 s4
-    (bitmap, s6)  <- readBytes (width * height) s5
-    Just (Graymap width height maxGray bitmap, s6)
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
 
 main :: IO ()
 main = do
@@ -143,6 +122,6 @@ main = do
   case args of
     [x] -> do
       src <- L.readFile x
-      let image = parseP5 src
+      let image = runParse parseP5 (ParseState src 0)
       print image
     _ -> putStrLn "Usage: PGM3 <image>"
