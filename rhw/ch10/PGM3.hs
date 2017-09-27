31917d944f16087c0bf230c780286ecf6254e39f
Emanuel Amaral Couto <unit73e@gmail.com>
2017-09-27 21:07:15 +0100

    Switch to monad implementation

--- rhw/ch10/PGM3.hs
+++ rhw/ch10/PGM3.hs
@@ -8,6 +8,7 @@ import Data.Int (Int64)
 import System.Environment (getArgs)
 import PGM(Graymap(..))
 import Control.Applicative ((<$>))
+import Control.Monad (ap, liftM)
 
 -- | A parse state
 data ParseState = ParseState
@@ -33,88 +34,89 @@ getState = Parse (\s -> Right (s, s))
 putState :: ParseState -> Parse ()
 putState s = Parse (\_ -> Right ((), s))
 
--- | Terminates parsing and returns an error
-bail :: String -> Parse a
-bail err = Parse $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
-
 assert :: Bool -> String -> Parse ()
-assert True _ = identity ()
-assert False err = bail err
-
--- | Chains parsers together
-(==>) :: Parse a -> (a -> Parse b) -> Parse b
-firstParser ==> secondParser = Parse chainedParser
-  where
-    chainedParser initState =
-      case runParse firstParser initState of
-        Left errMessage -> Left errMessage
-        Right (firstResult, newState) ->
-          runParse (secondParser firstResult) newState
-
-(==>&) :: Parse a -> Parse b -> Parse b
-p ==>& f = p ==> const f
+assert True _ = return ()
+assert False err = fail err
 
 instance Functor Parse where
-  fmap f parser = parser ==> \result -> identity (f result)
+  fmap = liftM
 
--- | Injects a value into a parser
-identity :: a -> Parse a
-identity a = Parse (\s -> Right (a, s))
+instance Applicative Parse where
+  pure = return
+  (<*>) = ap
+
+instance Monad Parse where
+  m >>= k =
+    Parse $ \s ->
+      case runParse m s of
+        Left errMessage -> Left errMessage
+        Right (a, s') -> runParse (k a) s'
+  return a = Parse $ \s -> Right (a, s)
+  fail err =
+    Parse $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
 
 parseByte :: Parse Word8
-parseByte =
-  getState ==> \initState ->
-    case L.uncons (string initState) of
-      Nothing -> bail "no more input"
-      Just (byte, remainder) -> putState newState ==> \_ -> identity byte
-        where newState = initState {string = remainder, offset = newOffset}
-              newOffset = offset initState + 1
+parseByte = do
+  initState <- getState
+  case L.uncons (string initState) of
+    Nothing -> fail "no more input"
+    Just (byte, remainder) -> do
+      let newState = initState {string = remainder, offset = newOffset}
+          newOffset = offset initState + 1
+      putState newState
+      return byte
 
 peekByte :: Parse (Maybe Word8)
 peekByte = (fmap fst . L.uncons . string) <$> getState
 
 parseWhile :: (Word8 -> Bool) -> Parse [Word8]
-parseWhile p =
-  (fmap p <$> peekByte) ==> \mp ->
-    if mp == Just True
-      then parseByte ==> \b -> (b :) <$> parseWhile p
-      else identity []
+parseWhile p = do
+  mp <- fmap p <$> peekByte
+  if mp == Just True
+    then parseByte >>= \b -> (b :) <$> parseWhile p
+    else return []
 
 parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
 parseWhileWith f p = fmap f <$> parseWhile (p . f)
 
 parseNat :: Parse Int
-parseNat =
-  parseWhileWith w2c isDigit ==> \digits ->
-    if null digits
-      then bail "no more input"
-      else let n = read digits
-           in if n < 0
-                then bail "integer overflow"
-                else identity n
+parseNat = do
+  digits <- parseWhileWith w2c isDigit
+  if null digits
+    then fail "no more input"
+    else let n = read digits
+         in if n < 0
+              then fail "integer overflow"
+              else return n
 
 parseBytes :: Int -> Parse L.ByteString
-parseBytes n =
-  getState ==> \st ->
-    let n' = fromIntegral n
-        (h, t) = L.splitAt n' (string st)
-        st' = st {offset = offset st + L.length h, string = t}
-    in putState st' ==>& assert (L.length h == n') "end of input" ==>&
-       identity h
+parseBytes n = do
+  st <- getState
+  let n' = fromIntegral n
+      (h, t) = L.splitAt n' (string st)
+      st' = st {offset = offset st + L.length h, string = t}
+  putState st'
+  assert (L.length h == n') "end of input"
+  return h
 
 parseP5 :: Parse Graymap
-parseP5 =
-  parseWhileWith w2c notWhite ==> \header -> skipSpaces ==>&
-  assert (header == "P5") "invalid raw header" ==>&
-  parseNat ==> \width -> skipSpaces ==>&
-  parseNat ==> \height -> skipSpaces ==>&
-  parseNat ==> \maxGrey ->
-  parseByte ==>&
-  parseBytes (width * height) ==> \bitmap ->
-  identity (Graymap width height maxGrey bitmap)
+parseP5 = do
+  header <- parseWhileWith w2c notWhite
+  skipSpaces
+  assert (header == "P5") "invalid raw header"
+  width <- parseNat
+  skipSpaces
+  height <- parseNat
+  skipSpaces
+  maxGrey <- parseNat
+  _ <- parseByte
+  bitmap <- parseBytes (width * height)
+  return (Graymap width height maxGrey bitmap)
 
 skipSpaces :: Parse ()
-skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+skipSpaces = do
+  _ <- parseWhileWith w2c isSpace
+  return ()
 
 main :: IO ()
 main = do
